3df09ab10a239e336a663aafbe0b726ac27babec
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-08-05 22:09:29 -0300

    Alterando o Carrousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from '../../components/Menu'
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carousel';
+import Carrousel from '../../components/Carrousel';
 import Footer from '../../components/Footer';
 
 function Home() {
@@ -16,28 +16,28 @@ function Home() {
         videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
       />
 
-      <Carousel
+      <Carrousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
 
-      <Carousel
+      <Carrousel
         category={dadosIniciais.categorias[1]}
       />
 
-      <Carousel
+      <Carrousel
         category={dadosIniciais.categorias[2]}
       />      
 
-      <Carousel
+      <Carrousel
         category={dadosIniciais.categorias[3]}
       />      
 
-      <Carousel
+      <Carrousel
         category={dadosIniciais.categorias[4]}
       />      
 
-      <Carousel
+      <Carrousel
         category={dadosIniciais.categorias[5]}
       />      
 
