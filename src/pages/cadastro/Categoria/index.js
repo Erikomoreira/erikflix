2ca51bb68ffddbb558b3b783ada73e2127422e90
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-08-09 10:34:31 -0300

    Ajuste no formulário de categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: ''
-  }
-  const [categorias, setCategorias] = useState([]);  
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
-  
-  function setValue(chave, valor){
+
+  function setValue(chave, valor) {
     setValues({
       ...values,
       [chave]: valor,
@@ -20,24 +21,66 @@ function CadastroCategoria() {
   }
 
   function handleChange(e) {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setValue(name, value);
   }
- 
+
+  useEffect(() => {
+    console.log('alo alo w brasil');
+    const URL_TOP = 'http://localhost:8080/categorias';
+
+    fetch(URL_TOP)
+      .then(async (respostaDoServidor) =>{
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+
+  // setTimeout(() => {
+  //   setCategorias([
+  //     ...categorias,
+  //             {
+  //           "id": 1,
+  //           "titulo": "Front End",
+  //           "cor": "#6BD1FF",
+  //           "link_extra": {
+  //             "text": "Formação de Front End na Alura",
+  //             "url": "https://www.alura.com.br/cursos-online-front-end"
+  //           }
+  //         },
+  //         {
+  //           "id": 2,
+  //           "titulo": "Back End",
+  //           "cor": "#00C86F",
+  //           "link_extra": {
+  //             "text": "Cursos de Back End na Alura",
+  //             "url": "https://www.alura.com.br/cursos-online-programacao"
+  //           }
+  //         },
+  //   ]);
+  //   }, 4 * 1000);
+  
+  }, []);
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
       <form onSubmit={function handleSubmit(e) {
         e.preventDefault();
         setCategorias([
-          ...categorias, 
-          values
+          ...categorias,
+          values,
         ]);
         setValues(valoresIniciais);
-      }}>
+      }}
+      >
 
-        <FormField 
+        <FormField
           label="Nome da Categoria"
           type="text"
           name="nome"
@@ -45,17 +88,15 @@ function CadastroCategoria() {
           onChange={handleChange}
         />
 
-
-        
-        <FormField 
+        <FormField
           label="Descrição"
           type="text"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
         />
-      
-      <FormField 
+
+        <FormField
           label="Cor"
           type="color"
           name="cor"
@@ -63,26 +104,30 @@ function CadastroCategoria() {
           onChange={handleChange}
         />
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
-        <ul>
-          {categorias.map((categoria, indice) => {
-              return (
-                <li key={`${categoria}${indice}`}>
-                  {categoria.nome}
-                </li>
-              )
-          })}
-        </ul>
+      {categorias.length === 0 && (
+      <div>
+        Loading...
+      </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
